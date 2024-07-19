29a0f20f284250454099eadfacdce045c9ffd2a6
JEP <juepark42@gmail.com>
2024-07-20 08:02:32 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
       <h1>Hi This is an</h1>
       <h1>App Router!</h1>
       <p>Let's trigger workflow</p>
-      <p>test delete files</p>
+      <p>update vercel</p>
     </div>
   );
 }
